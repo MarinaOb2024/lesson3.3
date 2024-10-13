d1c6303f9cdc8034b8bc464f22b33549cbff1a21
MarinaOb2024 <marinaobidova354@gmail.com>
2024-10-13 17:26:41 +0700

    add all cycle code

--- main.py
+++ main.py
@@ -13,16 +13,26 @@ icon = pygame.image.load("img/тир.jpeg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
-taget_widht = 80
-taget_height = 80
+target_width = 80
+target_height = 80
 
-target_x = random.randint(0, SCREEN_WIDTH - taget_widht)
-target_y = random.randint(0, SCREEN_HEIGHT - taget_height)
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
